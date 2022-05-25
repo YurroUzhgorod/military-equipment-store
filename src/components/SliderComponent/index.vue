199356c6590ab95b9f3a1a014f40308a180dfe03
Yuriy Kuliaba <yuriykuliaba@gmail.com>
2022-05-25 14:32:31 +0300

    added interval in carousell/slider

--- src/components/SliderComponent/index.vue
+++ src/components/SliderComponent/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-carousel hide-delimiters >
+  <v-carousel hide-delimiters cycle :interval="interval">
     <v-carousel-item
-      v-for="(item,i) in items"
+      v-for="(item, i) in items"
       :key="i"
       :src="item.src"
       cover
@@ -10,32 +10,29 @@
 </template>
 
 <script>
-  export default {
-    name:"SliderCompomemt",
-    data: () => ({
-      
-     
-        items: [
-          {
-            src: 'https://wallpaperaccess.com/full/751676.jpg',
-          },
-          {
-            src: 'https://ufpro.com/storage/app/media/Blog/Camouflage%20Patterns/blog-post-camouflage.jpg',
-          },
-          {
-            src: 'https://i.shgcdn.com/465d1a41-83d5-4706-9590-e1b67c2d07f7/-/format/auto/-/preview/3000x3000/-/quality/lighter/',
-          },
-          {
-            src: 'https://www.ghost-hood.com/myfactory/CustomUpload/374O357O340O370O356O369O350O338O352O369O366O326O364O353O327O/KATALOG_ausruestung.png',
-          },
-          {
-            src: 'https://i.cbc.ca/1.4702940.1528840553!/fileImage/httpImage/canadian-soldiers-participate-in-exercise-the-5th-canadian-division-support-base.JPG',
-          },
-        ],
-    }),
-  }
+export default {
+  name: "SliderCompomemt",
+  data: () => ({
+    interval: 5000,
+    items: [
+      {
+        src: "https://wallpaperaccess.com/full/751676.jpg",
+      },
+      {
+        src: "https://api.army.mil/e2/c/images/2016/03/02/425570/max1200.jpg",
+      },
+      {
+        src: "https://i.shgcdn.com/465d1a41-83d5-4706-9590-e1b67c2d07f7/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
+      },
+      {
+        src: "https://ichef.bbci.co.uk/news/976/cpsprodpb/14990/production/_124786348_nato.jpg",
+      },
+      {
+        src: "https://i.cbc.ca/1.4702940.1528840553!/fileImage/httpImage/canadian-soldiers-participate-in-exercise-the-5th-canadian-division-support-base.JPG",
+      },
+    ],
+  }),
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
